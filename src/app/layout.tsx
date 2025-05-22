ccf82e58e134c76a07df67e2eafdaa4dc542968c
Jimmy <nordstromjimmy@gmail.com>
2025-05-22 19:36:42 +0200

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
         <script
           defer
           data-domain="thinghy.com"
